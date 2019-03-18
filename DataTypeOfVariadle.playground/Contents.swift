3c85e795d8c94f8df2e1096bbbdcde86794c6298
Student10 <student10@Student10.local>
2019-03-18 14:28:12 +0700

    start DataType

---
+++ DataTypeOfVariadle.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" //นี่คือการประกาศ ที่อาศัยชนิดของ value บอก Datatype
+var mySurname: String="AAAAAA"//นี้คือการประกาศแบบกำหนด Datartype ตั้งแต่เกิด
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Explicti  Variable
+// การปะกายตัวแปร  นะจ๊ะ
+
+var myName = "pop"
+myName = "Doramon"
+
+//Explicti Constant
+let myFrend = "Nobita"
+//myFrend = "Sunako"Error เพรา  myFriendเป็นคำคงที่
+
+
+
