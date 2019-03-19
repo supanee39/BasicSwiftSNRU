a4f95b99ce0f7105030c47435a61996a9f3d8651
Student10 <student10@Student10.local>
2019-03-19 15:43:43 +0700

    Class and Function

--- DataTypeOfVariadle.playground/Contents.swift
+++ DataTypeOfVariadle.playground/Contents.swift
@@ -5,5 +5,37 @@ import UIKit
 var myName = "Doramon" //นี่คือการประกาศ ที่อาศัยชนิดของ value บอก Datatype
 var mySurname: String="AAAAAA"//นี้คือการประกาศแบบกำหนด Datartype ตั้งแต่เกิด
 
+// การแสดงผลบนConsole
+print("This is Console")
+print("myName=\(myName)")
+//lnteger Typcหรือ จำนวนที่ไม่มีเศษ
+var  number1=100
+var number2: Int = 200
+
+//Double Type หรือ จำนวนที่มีเศษ
+var number3=3.1416
+var numbr4: Double = 10.234
+
+//Boolean Type คือจำนวนที่มีแค่ค่า true, false
+var ststus = true
+var ststus2:Bool = false
+
+//Array Type
+var myFriends = ["Doramon","Nopita ","Sunako","Chisuka"]
+print("myFriends ที่มี index =1 มีค่าเท่ากับ => \(myFriends[1])")
+
+//การเพิ่มสมาชิคให้กับArray
+myFriends.append("MasterUNG")
+print(myFriends)
+
+//การลบสมาชิด ของ Array
+myFriends.remove(at: 1)
+print(myFriends)
+
+
+
+
+
+
 
 
